b85722cb430059389f3a960042a355cd388c0420
cosplaystone <srisshtiagarwal@gmail.com>
2021-05-27 02:42:17 +0530

    This is all i have created till now

---
+++ src/main/java/com/shallows/youbin/splashactivity.java
@@ -0,0 +1,34 @@
+package com.shallows.youbin;
+
+import android.content.Intent;
+import static java.lang.Thread.sleep;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class splashactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashactivity);
+
+        Thread thread = new Thread(){
+            public void run(){
+                try{
+                    sleep (4000);
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+                finally{
+                    Intent intent = new Intent(splashactivity.this, MainActivity.class);
+                    startActivity(intent);
+
+                }
+            }
+
+        };thread.start();
+    }
+}
